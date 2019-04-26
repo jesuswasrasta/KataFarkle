b143581e5bfc269d4dadd7e86751aff40149025e
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2019-04-26 18:08:32 +0200

    Throwing 1,2,4,4,6,6, scores 50

--- java/src/main/java/Throwing.java
+++ java/src/main/java/Throwing.java
@@ -1,9 +1,23 @@
-public class Throwing {
-    public Throwing(int d1, int d2, int d3, int d4, int d5, int d6) {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Throwing {
+    List<Integer> dice;
+
+    public Throwing(int d1, int d2, int d3, int d4, int d5, int d6) {
+        dice = new ArrayList<>();
+        dice.add(d1);
+        dice.add(d2);
+        dice.add(d3);
+        dice.add(d4);
+        dice.add(d5);
+        dice.add(d6);
     }
 
     public int score() {
-        return 0;
+        if (dice.contains(new Integer(1)))
+            return 50;
+        else
+            return 0;
     }
 }
