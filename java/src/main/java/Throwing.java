598c3d9ee03346ab451e1a41d621128fbc62baf1
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2019-04-26 18:03:54 +0200

    Throwing 2,2,4,4,6,6, scores 0

--- java/src/main/java/Throwing.java
+++ java/src/main/java/Throwing.java
@@ -2,4 +2,8 @@ public class Throwing {
     public Throwing(int d1, int d2, int d3, int d4, int d5, int d6) {
 
     }
+
+    public int score() {
+        return 0;
+    }
 }
--- java/src/test/java/ThrowingTest.java
+++ java/src/test/java/ThrowingTest.java
@@ -1,13 +1,17 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class ThrowingTest {
 
     @Test
-    public void working() {
-        Throwing throwing = new Throwing(2,2,4,4,6,6);
+    public void throwing224466ShoulScore0() {
+        int expectedScore = 0;
 
-        assertTrue(true);
+        Throwing throwing = new Throwing(2,2,4,4,6,6);
+        int actualScore = throwing.score();
+
+        assertEquals(expectedScore, actualScore);
     }
 }
