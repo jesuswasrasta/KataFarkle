59a1f0cc6609750100c39d4f730cafdce3ada030
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2019-04-26 18:01:57 +0200

    Create class for the dice throwing

---
+++ java/src/main/java/Throwing.java
@@ -0,0 +1,5 @@
+public class Throwing {
+    public Throwing(int d1, int d2, int d3, int d4, int d5, int d6) {
+
+    }
+}
--- java/src/test/java/ThrowingTest.java
+++ java/src/test/java/ThrowingTest.java
@@ -6,6 +6,7 @@ public class ThrowingTest {
 
     @Test
     public void working() {
+        Throwing throwing = new Throwing(2,2,4,4,6,6);
 
         assertTrue(true);
     }
